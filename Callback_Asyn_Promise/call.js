263edb157fabdff3b35fef7c83c613cb3ac25396
Shampa Khatun <shampacseruet@gmail.com>
2025-09-17 19:53:29 +0600

    promises practice with complex examples

--- Callback_Asyn_Promise/call.js
+++ Callback_Asyn_Promise/call.js
@@ -210,3 +210,33 @@ function asyncTask(success = true) {
 asyncTask(true)
   .then(result => console.log(result))
   .catch(error => console.error(error));
+
+
+  function myDisplayer(some) {
+    console.log(some);
+}
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 0;
+
+// some code (try to change x to 5)
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
+
+const myPromises = new Promise(function(myResolve, myReject) {
+  setTimeout(function(){ myResolve("I love You !!"); }, 20000);
+});
+
+myPromises.then(function(value) {
+  console.log(value); // I love You !!
+});
